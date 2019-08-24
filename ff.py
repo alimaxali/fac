ccd3c374a2996c69a85c94c735b490864f7273d2
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-08-24 20:13:05 +0200

    Add files via upload

---
+++ ff.py
@@ -0,0 +1,58 @@
+g='\033[1;32m'
+p='\033[1;35m'
+c='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+from os import system as gg
+import time
+sb = input("passwoed ")
+if sb == "999":
+ f = open("max.txt", "r")
+ for i in f:
+
+  print (g + "good" + c + "------> " + y + i)
+  time.sleep(4)
+  s = input("entar")
+  gg("payload")
+elif sb == "9090":
+ ii = "yasen.elrmady@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii) 
+elif sb == "111":
+ ii = "melsolia80@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "222":
+ ii = "ana.elkholy87@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "333":
+ ii = "masoud_neno@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "444":
+ ii = "ahmed.elzaiky@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "555":
+ ii = "petopeto77@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "666":
+ ii = "samirabo3nzaa@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "777":
+ ii = "aal_tayeb@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "888":
+ ii = "kanaria.kanaria@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+elif sb == "999":
+ ii = "faredsabry713@yahoo.com"
+ print (g + "good" + c + "------> " + y + ii)
+
+
+
+else:
+  print ("erorr") 
+  time.sleep(3)
+  gg("payload")
